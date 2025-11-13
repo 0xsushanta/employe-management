a755b0c1cd8ace59cfae0d71ad46d8cb913e97ee
sushantaruidas <sushantaruidas95@gmail.com>
2025-11-13 20:17:41 +0530

    Remove dist from repo

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const app = express();
-import dotenv from "dotenv";
-dotenv.config();
-app.listen(process.env.PORT, () => {
-    console.log("server is running on", process.env.PORT);
-});
-//# sourceMappingURL=index.js.map
