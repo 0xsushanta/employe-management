3472a508cf355e32717c0d80ca3d05f5410186fa
sushantaruidas <sushantaruidas95@gmail.com>
2025-11-13 23:06:11 +0530

    feat: add get employees endpoint

--- src/controller/employee.controller.ts
+++ src/controller/employee.controller.ts
@@ -46,3 +46,14 @@ export const viewEmployeeById = async (req:Request,res: Response)=>{
     }
     return res.status(200).json({data: employee})
 }
+export const getEmployees = async (req: Request,res: Response)=>{
+    const page= Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    const employees= await prismaClient.employee.findMany({
+        skip,
+        take: limit,
+        orderBy: {createdAt:"desc"}
+    })
+    res.json({ page, limit, data: employees });
+}
--- src/routes/employee.route.ts
+++ src/routes/employee.route.ts
@@ -1,6 +1,8 @@
 import express, { Router } from "express";
-import { addemplye, viewEmployeeById} from "../controller/employee.controller.js"
+import { addemplye, getEmployees, viewEmployeeById} from "../controller/employee.controller.js"
 const router: Router = express.Router();
 router.post("/add-employee",addemplye)
 router.get("/view-employees/:id",viewEmployeeById)
+router.get("/view-employees", getEmployees);
+
 export default router
